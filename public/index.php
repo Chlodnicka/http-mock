54a5dd674b080323c14d89a156424ba7b0218404
Chlodnicka <maja.chlodnicka@morele.net>
2021-08-19 21:39:55 +0200

    chore: remove outdated dependencies jeremeamia/superclosure, upgrade phpunit, add symfony/flex

--- public/index.php
+++ public/index.php
@@ -1,8 +1,30 @@
 <?php
-$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
-if (php_sapi_name() === 'cli-server' && is_file($filename)) {
-    return false;
+//$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+//if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+//    return false;
+//}
+//
+//$app = require __DIR__ . '/../src/app.php';
+//$app->run();
+
+
+use InterNations\Component\HttpMock\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
 }
 
-$app = require __DIR__ . '/../src/app.php';
-$app->run();
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
